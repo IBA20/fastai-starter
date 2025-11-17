11ca297e0e690d93b968684ed4e51a671698f4fa
iba <iba@imail.msk.ru>
2025-11-17 19:46:17 +0300

    /users/me endpoint added

--- src/routers/frontend.py
+++ src/routers/frontend.py
@@ -1,13 +1,35 @@
 from fastapi import APIRouter
+from fastapi.responses import JSONResponse
 
 router = APIRouter(prefix='/frontend-api')
 
 
 @router.get(
     '/hello',
-    summary='Поиск пользователей по имени',
-    response_description='Список найденных пользователей',
+    summary='Greetings string',
+    response_description='json containing "Hello World!"',
     tags=['Hello'],
 )
-async def hello():
+async def hello(current_user: str | None = None):
+    """
+    Returns Hello string\n
+    **current_user**: Current user name
+    """
     return {'message': 'Привет из бекенда!'}
+
+
+@router.post(
+    '/users/me',
+    summary='Получить учетные данные пользователя',
+    tags=['user'],
+)
+async def show_current_user():
+    user_data = {
+        'email': 'example@example.com',
+        'isActive': True,
+        'profileId': '1',
+        'registeredAt': '2025-06-15T18:29:56+00:00',
+        'updatedAt': '2025-06-15T18:29:56+00:00',
+        'username': 'user123',
+    }
+    return JSONResponse(content=user_data, status_code=200)
