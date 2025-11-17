bc14fdea96ec91c6a6bcce0595d92b8ac65cb22c
iba <iba@imail.msk.ru>
2025-11-17 18:17:52 +0300

    ruff settings updated

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from fastapi.staticfiles import StaticFiles
 
 from src.routers.frontend import router as frontend_router
 
-app = FastAPI()
+app = FastAPI(title='FastAI', description='AI website generator')
 
 app.include_router(router=frontend_router)
 app.mount('/', StaticFiles(directory='frontend/', html=True), name='site')
--- src/routers/frontend.py
+++ src/routers/frontend.py
@@ -3,6 +3,11 @@ from fastapi import APIRouter
 router = APIRouter(prefix='/frontend-api')
 
 
-@router.get('/hello')
+@router.get(
+    '/hello',
+    summary='Поиск пользователей по имени',
+    response_description='Список найденных пользователей',
+    tags=['Hello'],
+)
 async def hello():
     return {'message': 'Привет из бекенда!'}
