cc1dec4fe304ccc5a81f5e2a587bd10384d4b3f2
iba <iba@imail.msk.ru>
2026-05-26 22:48:30 +0300

    GotenbergServerError handling changed

--- src/generators.py
+++ src/generators.py
@@ -1,10 +1,10 @@
-import asyncio
 import logging
 
 import anyio
+from gotenberg_api import GotenbergServerError
 from html_page_generator import AsyncDeepseekClient, AsyncPageGenerator, AsyncUnsplashClient
 
-from src.screenshot_handler import save_screenshot
+from src.gotenberg_client import get_screenshot
 from src.settings import settings
 from src.storage import upload_file_to_s3
 
@@ -12,33 +12,39 @@ logger = logging.getLogger(__name__)
 
 
 async def page_generator(request, site_id, user_prompt: str):
-    async with (
-        AsyncUnsplashClient.setup(
-            settings.unsplash.api_key,
-            timeout=settings.unsplash.connection_timeout,
-        ),
-        AsyncDeepseekClient.setup(
-            settings.deepseek.api_key.get_secret_value(),
-            settings.deepseek.base_url.encoded_string(),
-            settings.deepseek.model,
-            timeout=settings.deepseek.connection_timeout,
-        ),
-    ):
-        generator = AsyncPageGenerator(debug_mode=settings.debug)
-        with anyio.CancelScope(shield=True):
-            async for chunk in generator(user_prompt):
-                yield chunk
+    try:
+        async with (
+            AsyncUnsplashClient.setup(
+                settings.unsplash.api_key,
+                timeout=settings.unsplash.connection_timeout,
+            ),
+            AsyncDeepseekClient.setup(
+                settings.deepseek.api_key.get_secret_value(),
+                settings.deepseek.base_url.encoded_string(),
+                settings.deepseek.model,
+                timeout=settings.deepseek.connection_timeout,
+            ),
+        ):
+            generator = AsyncPageGenerator(debug_mode=settings.debug)
+            with anyio.CancelScope(shield=True):
+                async for chunk in generator(user_prompt):
+                    yield chunk
 
-            await upload_file_to_s3(
-                request.app.state.client,
-                generator.html_page.html_code,
-                f'data/index_{site_id}.html',
-            )
-            logger.info('HTML успешно сохранён!')
-            asyncio.create_task(
-                save_screenshot(
+                await upload_file_to_s3(
                     request.app.state.client,
                     generator.html_page.html_code,
+                    f'data/index_{site_id}.html',
+                )
+                logger.info('HTML успешно сохранён!')
+                screenshot_bytes = await get_screenshot(generator.html_page.html_code)
+                if not screenshot_bytes:
+                    return
+                await upload_file_to_s3(
+                    request.app.state.client,
+                    screenshot_bytes,
                     f'data/screenshot_{site_id}.png',
-                ),
-            )
+                    mime_type='image/png',
+                )
+                logger.info('Скриншот успешно сохранён!')
+    except GotenbergServerError as exc:
+        logger.error('Failed to save screenshot: %s', exc)
--- src/gotenberg_client.py
+++ src/gotenberg_client.py
@@ -1,24 +1,20 @@
 import httpx
-from gotenberg_api import GotenbergServerError, ScreenshotHTMLRequest
+from gotenberg_api import ScreenshotHTMLRequest
 from httpx import Limits
 
 from src.settings import settings
-from src.storage import logger
 
 
 async def get_screenshot(raw_html: str) -> bytes | None:
-    try:
-        async with httpx.AsyncClient(
-            base_url=settings.gotenberg.base_url.encoded_string(),
-            timeout=settings.gotenberg.timeout,
-            limits=Limits(max_connections=settings.gotenberg.max_connections),
-        ) as client:
-            screenshot_bytes = await ScreenshotHTMLRequest(
-                index_html=raw_html,
-                width=settings.gotenberg.screenshot_width,
-                format=settings.gotenberg.screenshot_format,
-                wait_delay=settings.gotenberg.wait_delay,
-            ).asend(client)
-        return screenshot_bytes
-    except GotenbergServerError as exc:
-        logger.error('Failed to save screenshot: %s', exc)
+    async with httpx.AsyncClient(
+        base_url=settings.gotenberg.base_url.encoded_string(),
+        timeout=settings.gotenberg.timeout,
+        limits=Limits(max_connections=settings.gotenberg.max_connections),
+    ) as client:
+        screenshot_bytes = await ScreenshotHTMLRequest(
+            index_html=raw_html,
+            width=settings.gotenberg.screenshot_width,
+            format=settings.gotenberg.screenshot_format,
+            wait_delay=settings.gotenberg.wait_delay,
+        ).asend(client)
+    return screenshot_bytes
